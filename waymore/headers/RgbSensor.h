4859b3425f5940d5fd8fd96537d964b731f13797
crowcode17 <hcoke@sfsu.edu>
2024-12-16 18:40:46 -0800

    rgb sensor functionality

---
+++ waymore/headers/RgbSensor.h
@@ -0,0 +1,98 @@
+/**************************************************************
+* Class:: CSC-615-01 Spring 2024
+* Name:: Waymore Team
+* Student ID:: ...
+* Github-Name:: ...
+* Project:: Final Project
+*
+* File:: RgbSensor.h
+*
+* Description:: Declarations of functions and wrappers
+*		        for TCS34725 RGB sensor functionality.
+*
+**************************************************************/
+
+#ifndef _RGB_H_
+#define _RGB_H_
+
+// ============================================================================================= //
+// Library Linking
+// ============================================================================================= //
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <math.h> // used for fmin, fmax, and fmod. NOTE: need to compile with -lm
+// #include "WaymoreLib.h"
+
+// ============================================================================================= //
+// Definitions of Constants
+// ============================================================================================= //
+
+#define BUS "/dev/i2c-1"    // I2C bus device
+#define I2CADDR 0x29        // I2C address of the RGB sensor
+
+// ============================================================================================= //
+// Definitions of Types
+// ============================================================================================= //
+
+typedef enum TCS34725_Atime
+{
+    INTEGRATE2_4MS = 0XFF,
+    INTEGRATE24MS = 0xF6,
+    INTEGRATE50MS = 0xEB,
+    INTEGRATE101MS = 0xD5,
+    INTEGRATE154MS = 0xC0,
+    INTEGRATE700MS = 0x00,
+}Atime;
+
+typedef enum TCS34725_Gain
+{
+    GAIN1X = 0x00,
+    GAIN4X = 0x01,
+    GAIN16X = 0x02,
+    GAIN60X = 0x03,
+}Gain;
+
+typedef struct RGB
+{
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+}RGB;
+
+// ============================================================================================= //
+// Definitions of Structs
+// ============================================================================================= //
+
+typedef struct RGBSensorData{
+    
+} RGBSensorData;
+
+
+// ============================================================================================= //
+// Start and Stop Functions
+// ============================================================================================= //
+
+void startRGBSensor();
+void stopRGBSensor();
+
+// ============================================================================================= //
+// Functions intended for public use
+// ============================================================================================= //
+
+void initializeRGB(enum TCS34725_Atime, enum TCS34725_Gain);
+RGB collectRawReading();
+int checkRed();
+void uninitializeRGB();
+
+// ============================================================================================= //
+// End of File
+// ============================================================================================= //
+
+#endif
---
+++ waymore/source/RgbSensor.c
@@ -0,0 +1,292 @@
+/**************************************************************
+* Class:: CSC-615-01 Spring 2024
+* Name:: Waymore Team
+* Student ID:: ...
+* Github-Name:: ...
+* Project:: Final Project
+*
+* File:: RgbSensor.c
+*
+* Description:: Implementations of functions and wrappers
+*		        for TCS34725 RGB sensor functionality.
+*
+**************************************************************/
+
+// ============================================================================================= //
+// Library Linking
+// ============================================================================================= //
+
+#include "RgbSensor.h"
+
+// ============================================================================================= //
+// Internal Variables & States
+// ============================================================================================= //
+
+int device = -1;
+int readRegister;
+
+Thread * RGBSensorThread;
+RGBSensorData * rgbSensorData;
+
+enum TCS34725_Atime integrationTime;
+
+RGB whiteRef;
+RGB blackRef;
+
+// ============================================================================================= //
+// Main Loop & Business Logic
+// ============================================================================================= //
+
+void * RGBSensorThreadLoop(void * args)
+{
+    (void)args;
+
+    while (RGBSensorThread->running)
+    {
+        microWait(1);
+    }
+
+    return NULL;
+}
+
+// ============================================================================================= //
+// Start and Stop Functions
+// ============================================================================================= //
+
+void startRGBSensor()
+{
+    RGBSensorThread = startThread("RGB sensor thread", RGBSensorThreadLoop);
+
+    if(RGBSensorThread == NULL)
+    {
+        fprintf(stderr, "Failed to start the RGB sensor thread. Exiting.\n");
+        exit(1);
+    }
+}
+
+void stopRGBSensor()
+{
+    stopThread(RGBSensorThread);
+    RGBSensorThread = NULL;
+}
+
+
+// ============================================================================================= //
+// Private Helper functions
+// ============================================================================================= //
+
+void waitForIntegration()
+{
+    switch (integrationTime)
+    {
+        case INTEGRATE2_4MS:
+            usleep(2400);
+            break;
+        case INTEGRATE24MS:
+            usleep(24000);
+            break;
+        case INTEGRATE50MS:
+            usleep(50000);
+            break;
+        case INTEGRATE101MS:
+            usleep(101000);
+            break;
+        case INTEGRATE154MS:
+            usleep(154000);
+            break;
+        case INTEGRATE700MS:
+            usleep(700000);
+            break;
+    }
+}
+
+void configureRGB(enum TCS34725_Atime atime, enum TCS34725_Gain gain)
+{
+    // Initialize a temp variable to store hex configuration settings
+    char config[2] = {0};
+
+    // Put Enable register (0x80) into config[0]
+    config[0] = 0x80;
+    config[1] = 0x03;
+    if (write(device, config, 2) != 2) {
+        fprintf(stderr, "ConfigureRGB: Failed to write to I2C device!\n");
+        close(device);
+        exit(1);
+    }
+
+    // Put ALS time register (0x81) into config[0]
+    config[0] = 0x81;
+    // Set the integration time
+    config[1] = atime;
+    // Write 2 bytes of data to the device
+    if (write(device, config, 2) != 2) {
+        fprintf(stderr, "ConfigureRGB: Failed to write to I2C device!\n");
+        close(device);
+        exit(1);
+    }
+
+    // Put Wait Time register (0x83) into config[0]
+    config[0] = 0x83;
+    // Set the wait time to 2.4 ms
+    config[1] = 0xFF;
+    // Write 2 bytes of data to the device
+    if (write(device, config, 2) != 2) {
+        fprintf(stderr, "ConfigureRGB: Failed to write to I2C device!\n");
+        close(device);
+        exit(1);
+    }
+
+    // Put Control register(0x8F) into config[0]
+    config[0] = 0x8F;
+    // Set the gain to 4x
+    config[1] = gain;
+    // Write 2 bytes of data to the device
+    if (write(device, config, 2) != 2) {
+        fprintf(stderr, "ConfigureRGB: Failed to write to I2C device!\n");
+        close(device);
+        exit(1);
+    }
+
+    // Need this in memory for reading
+    readRegister = 0x94;
+}
+
+// ============================================================================================= //
+// Function intended for external use
+// ============================================================================================= //
+
+void initializeRGB(enum TCS34725_Atime integrationTime, enum TCS34725_Gain gain)
+{
+    // Open the I2C bus once and set the device
+    if ((device = open(BUS, O_RDWR)) < 0) {
+        fprintf(stderr, "initializeRGB: Failed to open the bus.\n");
+        exit(1);
+    }
+
+    if (ioctl(device, I2C_SLAVE, I2CADDR) < 0) {
+        fprintf(stderr, "initializeRGB: Failed to set I2C address.\n");
+        close(device);
+        exit(1);
+    }
+
+    // Configure the RGB sensor
+    configureRGB(integrationTime, gain);
+
+    // Wait for sensor initialization
+    usleep(500000);
+}
+
+RGB collectRawReading()
+{
+    if (device < 0)
+    {
+        fprintf(stderr, "RGB device has not been initialized!\n");
+        exit(1);
+    }
+
+    // Init an array to store raw data
+    char data[8] = {0};
+
+    // Trigger a read
+    if (write(device, &readRegister, 1) != 1) {
+        fprintf(stderr, "Error: Failed to write to I2C device.\n");
+        close(device);
+        exit(1);
+    }
+
+    // Wait for the integration time to pass
+    waitForIntegration();
+
+    // Read the results
+    if(read(device, data, 8) != 8)
+    {
+        fprintf(stderr, "Ran into error while reading from RGB sensor\n");
+        close(device);
+        exit(1);
+    }
+
+    // Put the data into the correct bins
+    uint16_t clear = (data[1] << 8) + data[0];
+    uint16_t red   = (data[3] << 8) + data[2];
+    uint16_t green = (data[5] << 8) + data[4];
+    uint16_t blue  = (data[7] << 8) + data[6];
+
+    // Calculate the RGB values
+    int r = ((float)red / clear) * 255.0;
+    int g = ((float)green / clear) * 255.0;
+    int b = ((float)blue / clear) * 255.0;
+    
+    // Bound all readings from 0 to 255
+    r = r > 255 ? 255 : ((r < 0) ? 0 : r);
+    g = g > 255 ? 255 : ((g < 0) ? 0 : g);
+    b = b > 255 ? 255 : ((b < 0) ? 0 : b);
+
+    // Contruct the RGB struct and return
+    RGB rawReading;
+
+    rawReading.red = (uint8_t) r;
+    rawReading.green = (uint8_t) g;
+    rawReading.blue = (uint8_t) b;
+
+    return rawReading;
+}
+
+int checkRed(RGB reading)
+{
+    // convert to HSV
+    // acceptable degrees for red: greater than 330 (pink) or less than 30 (orange)
+    // we don't really need S or V, we just need H (hue)
+    // we're keeping it because it's good for checking
+
+    // divide r,g,b by 255 to change the range from 0..255 to 0.0...1.0 
+    double r = reading.red / 255.0;
+    double g = reading.green / 255.0;
+    double b = reading.blue / 255.0;
+
+    double cmax = fmax(r, fmax(g, b)); // maximum of r, g, b 
+    double cmin = fmin(r, fmin(g, b)); // minimum of r, g, b 
+    double diff = cmax - cmin; // diff of cmax and cmin. 
+    double h = -1, s = -1; 
+
+    // if cmax and cmax are equal then h = 0 
+    if (cmax == cmin) 
+        h = 0; 
+  
+    // if cmax equal r then compute h 
+    else if (cmax == r) 
+        h = 60 * ((g - b) / diff) + 360 % 360; 
+  
+    // if cmax equal g then compute h 
+    else if (cmax == g) 
+        h = fmod(60 * ((b - r) / diff) + 120, 360); 
+  
+    // if cmax equal b then compute h 
+    else if (cmax == b) 
+        h = fmod(60 * ((r - g) / diff) + 240, 360); 
+  
+    // if cmax equal zero 
+    if (cmax == 0) 
+        s = 0; 
+    else
+        s = (diff / cmax) * 100; 
+  
+    // compute v 
+    double v = cmax * 100; 
+
+    printf("HSV = %f %f %f\n", h, s, v);
+
+    if(h < 30 || h > 330)
+        return 1;
+
+    return 0;
+}
+
+void uninitializeRGB()
+{
+    // Close the I2C bus
+    close(device);
+}
+
+// ============================================================================================= //
+// End of File
+// ============================================================================================= //
