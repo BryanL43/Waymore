053da1e0cdf977d641cdc218952b967f176b842d
Bryan Lee <bryanlee56098@gmail.com>
2025-07-18 20:50:55 -0700

    Archive cleanup

--- waymore/headers/FrameProcessor.hpp
+++ waymore/headers/FrameProcessor.hpp
@@ -1,23 +1,23 @@
-// #ifndef _FRAME_PROCESSOR_HPP_
-// #define _FRAME_PROCESSOR_HPP_
+#ifndef _FRAME_PROCESSOR_HPP_
+#define _FRAME_PROCESSOR_HPP_
 
-// #include <iostream>
-// #include <opencv2/opencv.hpp>
-// #include <vector>
-// #include "Camera.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include "Camera.h"
 
-// class FrameProcessor {
-//     public:
-//         FrameProcessor();
-//         void processFrame(const uint8_t* buffer, CameraData * cameraData);
-//         ~FrameProcessor();
+class FrameProcessor {
+    public:
+        FrameProcessor();
+        void processFrame(const uint8_t* buffer, CameraData * cameraData);
+        ~FrameProcessor();
 
-//     private:
-//         cv::Point * contourcenters;
-//         cv::Mat erosionKernel;
-//         cv::Mat dilationKernel;
+    private:
+        cv::Point * contourcenters;
+        cv::Mat erosionKernel;
+        cv::Mat dilationKernel;
 
-//         double processSlice(cv::Mat &slice, int sliceIndex);
-// };
+        double processSlice(cv::Mat &slice, int sliceIndex);
+};
 
-// #endif
+#endif
